3caa0137c7e63e76f190dc1002e862be6bdc1915
rubber ducky <warmcuppatea@duck.com>
2022-09-06 22:07:02 +0800

    rbgupdate

--- archive-code.R
+++ archive-code.R
@@ -9,3 +9,16 @@ toc: true
 toc_depth: 2
 toc_float:
   collapsed: false
+
+![](./pics/flower-1.jpg)
+
+### Shasta Daisy?
+
+![](./pics/flower-2.JPG)
+<p align = "center">
+  Shasta daisies
+(ƒ/8,
+  1/1000,
+  72 mm,
+  ISO125)
+</p>
